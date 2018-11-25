86e5e9525f9e2e28c49a5c4bd0758715920f2590
Ahmad <ahmad.zeidan@outlook.fr>
2018-11-25 20:13:52 +0100

    init projet

---
+++ src/Repository/PropertyRepository.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Picture;
+use App\Entity\Property;
+use App\Entity\PropertySearch;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Property|null find(Property[]$id, $lockMode = null, $lockVersion = null)
+ * @method Property|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Property[]    findAll()
+ * @method Property[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PropertyRepository extends ServiceEntityRepository
+{
+    /**
+     * @var PaginatorInterface
+     */
+    private $paginator;
+
+    public function __construct(RegistryInterface $registry, PaginatorInterface $paginator)
+    {
+        parent::__construct($registry, Property::class);
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * @return PaginationInterface
+     */
+    public function paginateAllVisible(PropertySearch $search, int $page): PaginationInterface
+    {
+        $query = $this->findVisibleQuery();
+
+        if ($search->getMaxPrice()) {
+            $query = $query
+                ->andWhere('p.price <= :maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+        }
+
+        if ($search->getMinSurface()) {
+            $query = $query
+                ->andWhere('p.surface >= :minsurface')
+                ->setParameter('minsurface', $search->getMinSurface());
+        }
+
+        if ($search->getLat() && $search->getLng() && $search->getDistance()) {
+            $query = $query
+                ->andWhere('(6353 * 2 * ASIN(SQRT( POWER(SIN((p.lat - :lat) *  pi()/180 / 2), 2) +COS(p.lat * pi()/180) * COS(:lat * pi()/180) * POWER(SIN((p.lng - :lng) * pi()/180 / 2), 2) ))) <= :distance')
+                ->setParameter('lng', $search->getLng())
+                ->setParameter('lat', $search->getLat())
+                ->setParameter('distance', $search->getDistance());
+        }
+
+        if ($search->getOptions()->count() > 0) {
+            $k = 0;
+            foreach($search->getOptions() as $option) {
+                $k++;
+                $query = $query
+                    ->andWhere(":option$k MEMBER OF p.options")
+                    ->setParameter("option$k", $option);
+            }
+        }
+
+        $properties = $this->paginator->paginate(
+            $query->getQuery(),
+            $page,
+            12
+        );
+
+
+        $this->hydratePicture($properties);
+
+        return $properties;
+    }
+
+    /**
+     * @return Property[]
+     */
+    public function findLatest(): array
+    {
+        $properties = $this->findVisibleQuery()
+            ->setMaxResults(4)
+            ->getQuery()
+            ->getResult();
+        $this->hydratePicture($properties);
+        return $properties;
+    }
+
+    private function findVisibleQuery(): QueryBuilder
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.sold = false');
+    }
+
+    private function hydratePicture($properties) {
+        if (method_exists($properties, 'getItems')) {
+            $properties = $properties->getItems();
+        }
+        $pictures = $this->getEntityManager()->getRepository(Picture::class)->findForProperties($properties);
+        foreach($properties as $property) {
+            /** @var $property Property */
+            if($pictures->containsKey($property->getId())) {
+                $property->setPicture($pictures->get($property->getId()));
+            }
+        }
+    }
+}
